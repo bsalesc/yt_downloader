87ef9d1fbc9ce2f2dade9d859c2355325bd12943
bsalesc <bsales.c@gmail.com>
2019-01-11 13:09:36 +0100

    Adjust to use webpack-dev-server instead of node server manually

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
+import { useState } from 'react';
 
-export class Welcome extends React.Component {
-  render() {
-    return <h1>Welcome to downloader</h1>;
-  }
-}
+export const Welcome = () => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <React.Fragment>
+      <h1>Welcome to downloader.</h1>
+      <button onClick={() => setShow(!show)}>Ok</button>
+      {!show || <h2>:)</h2>}
+    </React.Fragment>
+  );
+};
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,31 +1,46 @@
-const path = require('path'),
-  webpack = require('webpack'),
-  HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const env = process.env.NODE_ENV;
 
-module.exports = {
+const getPlugins = env => {
+  const plugins = [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') })];
+  if (env === 'dev') plugins.push(new webpack.HotModuleReplacementPlugin());
+
+  return plugins;
+};
+
+const getRules = env => {
+  const rules = [{ test: /\.(ts|tsx)$/, loader: 'ts-loader' }];
+  if (env === 'dev') rules.push({ enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' });
+
+  return rules;
+};
+
+const config = {
   entry: {
-    app: ['./src/App.tsx', 'webpack-hot-middleware/client'],
+    app: ['./src/App.tsx'],
     vendor: ['react', 'react-dom']
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/[name].bundle.js'
   },
-  devtool: 'source-map',
+  devtool: 'none',
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
   },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        loader: 'ts-loader'
-      },
-      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') }),
-    new webpack.HotModuleReplacementPlugin()
-  ]
+  module: { rules: getRules(env) },
+  plugins: getPlugins(env)
 };
+
+if (env === 'dev') {
+  config.devtool = 'source-map';
+  config.devServer = {
+    contentBase: path.join(__dirname, 'dist'),
+    hot: true,
+    historyApiFallback: true
+  };
+}
+
+module.exports = config;
